05b956c550ed5736915980d959f3af0c7c6b9872
Kahalia Stanberry <kahalia.stanberry@gmail.com>
2023-11-30 21:50:33 -0500

    image added with media queries

--- client/components/Landing.jsx
+++ client/components/Landing.jsx
@@ -1,23 +1,30 @@
 import React from "react";
 import Journal from "./Journal";
+import landingImg from "../../assets/ld-img.png";
+import "../style/Landing.css";
 import Affirmation from "./Affirmation";
-import { useNavigate } from "react-router-dom"; 
+import { useNavigate } from "react-router-dom";
+
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
 
 const Landing = () => {
-    const navigate = useNavigate(); 
+  const navigate = useNavigate();
 
-    function handleClick(){
-        navigate("/journal"); 
-    }
-    return (
-    <div>
-        Landings
-        <button type="button" onClick={handleClick}>
-            Go home
-        </button>
-        <Affirmation/>
+  function handleClick() {
+    navigate("/journal");
+  }
+  return (
+    <div class="landing-container">
+      <h1>Gratitouille</h1>
+
+      <img src={landingImg} alt="four cartoon rats surrounding a heart"id="landing-img" />
+      <Button type="button" onClick={handleClick}>
+        Go home
+      </Button>
+      <Affirmation />
     </div>
-    )
+  );
 };
 
-export default Landing; 
+export default Landing;
